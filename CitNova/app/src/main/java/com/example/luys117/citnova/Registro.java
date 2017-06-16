208abcb23ae067eeb14012ac3bbb86b5c8a32b06
Francisco Hernández <luisz1@hotmail.es>
2017-06-15 22:09:52 -0500

    Autentificación con FireBase

--- CitNova/app/src/main/java/com/example/luys117/citnova/Registro.java
+++ CitNova/app/src/main/java/com/example/luys117/citnova/Registro.java
@@ -1,13 +1,85 @@
 package com.example.luys117.citnova;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Registro extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    EditText nombre,correo,contraseña,motivo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+        nombre=(EditText) findViewById(R.id.nombre);
+        correo=(EditText) findViewById(R.id.correo);
+        contraseña=(EditText) findViewById(R.id.contraseña);
+        motivo=(EditText) findViewById(R.id.motivo);
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d("lol", "onAuthStateChanged:signed_in:" + user.getUid());
+                    Intent go=new Intent(getApplicationContext(),EleccionDeChat.class);
+                    startActivity(go);
+                } else {
+                    // User is signed out
+                    Log.d("lel", "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
     }
-}
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    private void createAccount(String correo,String contra){
+        mAuth.createUserWithEmailAndPassword(correo, contra)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d("lal", "createUserWithEmail:onComplete:" + task.isSuccessful());
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(Registro.this,"No existes viejo :/",Toast.LENGTH_SHORT).show();
+                        }
+
+                        // ...
+                    }
+                });
+
+    }
+
+}/*Fin de la clase*/
